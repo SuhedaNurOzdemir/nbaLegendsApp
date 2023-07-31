e34bd4dbce48288bd3442e14cfca83a70934f934
SuhedaNurOzdemir <suhedanurozd99@gmail.com>
2023-07-31 22:45:32 +0300

    react

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,31 @@
+import Container from "react-bootstrap/Container";
+import { data } from "../helpers/data";
+import PlayerCard from "./PlayerCard";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState } from "react";
+
+const CardContainer = () => { 
+  const [search, setSearch] = useState("")
+  return (
+    <>
+      <Form.Control  
+      placeholder="Search"  
+      className="w-50 m-auto"  
+      type="search"  
+      onChange={(e) => setSearch(e.target.value)} />
+      <Container className="card-container rounded-4 my-4 p-3 ">
+        <Row className="g-3 justify-content-center">
+          {data.filter((player) => player.name.toLowerCase().includes(search.trim().toLowerCase()))  .map((player, i) => (
+            <Col xl={3} md={6} lg={4} key={i}>
+              <PlayerCard {...player} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default CardContainer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import  Container from "react-bootstrap/Container";
+import nbaLogo from "../assets/nba-logo.png"
+import Image from 'react-bootstrap/Image';
+
+
+const Header = () => {
+  return ( 
+    <Container> 
+      <Image src={nbaLogo} width="200px"></Image> 
+      <h1 className="my-2 display-4">NBA Legends</h1>
+    </Container>
+
+  )
+}
+
+export default Header;
